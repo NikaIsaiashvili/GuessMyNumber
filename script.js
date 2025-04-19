305aa620962e94a8154d605fc771a50aa223141a
NikaIsaiashvili <nisaiashvili@gmail.com>
2025-04-19 17:12:50 +0400

    Update script.js
    added secret number,  variations, win, lose, number too high, too low, background changes green if win, red if lost

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
 'use strict';
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = '🎉 Correct Number!';
 
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
+//Defining Secret Number
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+console.log(secretNumber, typeof secretNumber);
 
-// document.querySelector('.guess').value = 23;
-// console.log(document.querySelector('.guess').value);
+let score = 20;
 
 // Handling Click Events
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
 
   // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No Number!';
+
+    // when player wins
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+    document.querySelector('main').style.backgroundColor = 'green';
+
+    // when guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 too high!';
+      // decrement score
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      // when player loses
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('main').style.backgroundColor = 'red';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // when guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 too low!';
+      // decrement score
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      // when player loses
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('main').style.backgroundColor = 'red';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
